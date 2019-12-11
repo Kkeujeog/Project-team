001b14cedf233a07a367b212c00c77d6a0ac8ecd
yoonahlim <limyoonah05@gmail.com>
2019-12-11 18:37:27 +0900

    윤아 삭제 커밋

---
+++ python code/npy파일 생성 코드(RGB)_ 윤아수정.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:408cd42816eccdee964e5f06db204c6ed97ffd6226c56ae04ae5a2dc845ff6b6
+size 1488
---
+++ python code/rotation코드_윤아수정.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:01f15abe58a20e8022fb7b9da05679966ec103b94ad8e3e5ef5b4714a8278660
+size 1693
